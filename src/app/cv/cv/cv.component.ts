9fa4cd914905835edcb53b53285a875a3eac421d
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-16 15:49:40 +0100

    cache Obsevable response with share et shareReplay

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { Cv } from "../model/cv";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
-import { Observable, catchError, of, retry } from "rxjs";
+import { Observable, catchError, map, of, retry, shareReplay } from "rxjs";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
@@ -10,6 +10,8 @@ import { Observable, catchError, of, retry } from "rxjs";
 })
 export class CvComponent {
   cvs$: Observable<Cv[]>;
+  juniors$: Observable<Cv[]>;
+  seniors$: Observable<Cv[]>;
   nbClick = 0;
   date = new Date();
   constructor(private toastr: ToastrService, private cvService: CvService) {
@@ -24,7 +26,14 @@ export class CvComponent {
             Attention!! Les données sont fictives, problème avec le serveur.
             Veuillez contacter l'admin.`);
         return of(this.cvService.getFakeCvs());
-      })
+      }),
+      shareReplay()
+    );
+    this.juniors$ = this.cvs$.pipe(
+      map((cvs) => cvs.filter((cv) => cv.age < 40))
+    );
+    this.seniors$ = this.cvs$.pipe(
+      map((cvs) => cvs.filter((cv) => cv.age >= 40))
     );
   }
 }
