877a1d01f42641a61666f0efb5035bf03222154b
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-16 16:03:49 +0100

    unsubscribe example

--- src/app/components/test-observable/test-observable.component.ts
+++ src/app/components/test-observable/test-observable.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { ToastrService } from "ngx-toastr";
-import { Observable, filter, map, take } from "rxjs";
+import { Observable, Subscription, filter, map, take } from "rxjs";
 
 @Component({
   selector: "app-test-observable",
   templateUrl: "./test-observable.component.html",
   styleUrls: ["./test-observable.component.css"],
 })
-export class TestObservableComponent {
+export class TestObservableComponent implements OnDestroy {
   monObservable: Observable<number>;
-
+  mySubbscriptions!: Subscription;
   constructor(private toaster: ToastrService) {
     this.monObservable = new Observable((observer) => {
       let i = 5;
@@ -21,30 +21,37 @@ export class TestObservableComponent {
         observer.next(i--);
       }, 1000);
     });
-    this.monObservable.subscribe({
-      next: (val) => {
-        console.log(val);
-      },
-    });
-    this.monObservable
-      /* 5 4 3 2 1 */
-      .pipe(
-        map((x) => x * 3),
-        /* 15 12 9 6 3 */
-        filter((x) => !(x % 2)),
-        /* 12 6 */
-        take(1)
-      )
-      .subscribe({
+    this.mySubbscriptions.add(
+      this.monObservable.subscribe({
         next: (val) => {
-          this.toaster.info("" + val);
+          console.log(val);
         },
-        error: (e) => {
-          console.log(e);
-        },
-        complete: () => {
-          this.toaster.warning("C'est fini !!");
-        },
-      });
+      })
+    );
+    this.mySubbscriptions.add(
+      this.monObservable
+        /* 5 4 3 2 1 */
+        .pipe(
+          map((x) => x * 3),
+          /* 15 12 9 6 3 */
+          filter((x) => !(x % 2)),
+          /* 12 6 */
+          take(1)
+        )
+        .subscribe({
+          next: (val) => {
+            this.toaster.info("" + val);
+          },
+          error: (e) => {
+            console.log(e);
+          },
+          complete: () => {
+            this.toaster.warning("C'est fini !!");
+          },
+        })
+    );
+  }
+  ngOnDestroy(): void {
+    this.mySubbscriptions.unsubscribe();
   }
 }
